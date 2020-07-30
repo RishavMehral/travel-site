c8360cf100547ea065fa9d4ae44871ae56814b33
Rishav <rishavmehral@icloud.com>
2020-07-31 00:45:41 +0530

    completed sticky header functionality.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,9 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
+import StickyHeader from './modules/StickyHeader'
+
+let stickHeader = new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".row__medium-4"), 60)
 new RevealOnScroll(document.querySelectorAll(".generic-content-container"), 60)
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
